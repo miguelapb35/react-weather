6a717e68920663888f62cf0081f0fbf67a430b42
Miguel Angel <miguelapb35@gmail.com>
2020-04-25 18:10:39 -0500

    Updates

---
+++ precache-manifest.416edc452074de51a8ffb5f2acb2798e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b5409eba58fa76d0d0debe887adce1e",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "cbf46e1b0aa8c30d397f",
+    "url": "/react-weather/static/css/main.f9b3a264.chunk.css"
+  },
+  {
+    "revision": "1438712fc7888e3c033b",
+    "url": "/react-weather/static/js/2.44bafd64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather/static/js/2.44bafd64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf46e1b0aa8c30d397f",
+    "url": "/react-weather/static/js/main.cd163c87.chunk.js"
+  },
+  {
+    "revision": "c3685a00a7f06dddd998",
+    "url": "/react-weather/static/js/runtime-main.6ee29a46.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/react-weather/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/react-weather/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
